77bdf8b769c6367e8e83b20844e7afaa0eb2b1f2
doncel9 <33489452+doncel9@users.noreply.github.com>
2017-11-08 16:55:30 +0100

    Cambios Modulador

--- moduladorBFSK.m
+++ moduladorBFSK.m
@@ -1,29 +1,49 @@
 % function [ output_args ] = moduladorBFSK( input_args )
 
 %% TX
-x = [0 0 0 1 0 0 1 0 1];
-Rb = 563.48;
+x = [0 0 1 0 1 0];
+Rb = 564.48; %bps
+
+% Frecuencias de Simbolo
 f0 = 8*Rb;
 f1 = 7*Rb;
+
+% Periodos
+T0 = 1/f0;
+T1 = 1/f1;
+
+% Frecuancia Portadora y Muestreo
 fc = 7.5*Rb;
 fs = 56*Rb;
+Ts = 1/fs;
 
 Tb = 1/Rb;
-t = 0:Tb/fs:Tb;
+t = 0:(Ts):(Tb-Ts);
 s = [];
-phi = 0;
-figure
+
+dPhi1 = (2*pi)/(fs*T1);
+dPhi0 = (2*pi)/(fs*T0);
+
+phaseArray = 0;
+
 for i = 1:length(x)
     if(x(i) == 1)
-        s = [s cos(2*pi*f1*t+ phi)];
+        p = phaseArray(end) + dPhi1*(1:length(t));
+        phaseArray = [phaseArray p];
     elseif(x(i) == 0)
-        s = [s cos(2*pi*f0*t+ phi)];
+        p = phaseArray(end) + dPhi0*(1:length(t));
+        phaseArray = [phaseArray p];
     end
-    plot(s)
-    hold on
 end
+phaseArray=wrapTo2Pi(phaseArray);
+
+s = cos(phaseArray);
+figure
+plot(s)
 
 %% CHANNEL
 h = 1;
 w = 0;
 y=h.*s+w;
+
+%% RX
