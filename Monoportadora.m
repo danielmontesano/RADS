64dd452a92a22f70602a4e905bbe5a0e9f36eb61
doncel9 <33489452+doncel9@users.noreply.github.com>
2017-11-30 00:08:18 +0100

    Cambios PLL y añadir PLL2 con Bucle de Costas

--- Monoportadora.m
+++ Monoportadora.m
@@ -1,26 +1,31 @@
 clear all
+close all
 clc
 
 %% TX
-x = [0 1 0 1 0 0];
+% x = [1 1 1 1 1 1];
+x = ones(1,1000);
 Rb = 564.48; %bps
 
 s = moduladorBFSK(x,Rb);
 
 %% CHANNEL
+
 h = 1;
-w = 1*randn(1,length(s));
+w = 0.01*randn(1,length(s));
 y=h.*s+w;
 
 %% RX
-% [ y_dem ] = demoduladorBFSK(y, Rb, 1);
 [ y_dem ] = demoduladorBFSK(y, Rb, 4);
-% display(['Received Signal: ' num2str(y_dem)])
-
 figure(1)
 subplot(3,1,1)
 plot(s);
+title('Señal Original')
 subplot(3,1,2)
 plot(y)
+title('Señal Recibida')
 subplot(3,1,3)
 plot(y_dem)
+title('Señal Demodulada')
+
+display(['Received Signal: ' num2str(y_dem)])
--- demoduladorBFSK.m
+++ demoduladorBFSK.m
@@ -2,7 +2,7 @@ function [ y_dem ] = demoduladorBFSK(y, Rb, modo)
 
 % Frecuencias de Simbolo
 f0 = 8*Rb;
-f1 = 2*Rb;
+f1 = 7*Rb;
 
 fc=(f0+f1)/2;
 
@@ -34,71 +34,65 @@ if(modo == 1)
         y_dem=[y_dem a];
     end
 elseif(modo == 2)
-    % A IMPLEMENTAR
-elseif(modo == 3)
-    % A IMPLEMENTAR   
-elseif(modo == 4)
-    Bn = 25; 
-    y_dem =[];
+    %     F0_l=fc-175-((564.48/2)*(1+0.7));
+    %     F0_h=fc+175-((564.48/2)*(1-0.7));
+    %     F1_l=fc-175+((564.48/2)*(1-0.7));
+    %     F1_h=fc+175+((564.48/2)*(1+0.7));
+
+        F1_l=fc-Rb/2-Rb/2;
+        F1_h=fc-Rb/2+Rb/2;
+        F0_l=fc+Rb/2-Rb/2;
+        F0_h=fc+Rb/2+Rb/2;
+
+    N      = 4;         % Order
+    Apass  = 1;         % Passband Ripple (dB)
+    Astop  = 10;        % Stopband Attenuation (dB)
+
+    % Construct an FDESIGN object and call its ELLIP method.
+    h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F0_l, F0_h, ...
+                          Astop, Apass, Astop, fs);
+    bpf_0 = design(h, 'ellip');
+    h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F1_l, F1_h, ...
+                          Astop, Apass, Astop, fs);
+    bpf_1 = design(h, 'ellip');
+
     sLen = length(t);
-    for i=sLen:sLen:length(y)
-        [cos_dem0,error0] = pll(y((i-sLen+1):i), f0, fs, Bn);
-        [cos_dem1,error1] = pll(y((i-sLen+1):i), f1, fs, Bn);
-        if(sum(error0)<sum(error1))
-            y_dem = [y_dem cos_dem0];
-        else
-            y_dem = [y_dem cos_dem1];
+    y_dem =[];
+
+     for i=sLen:sLen:length(y)
+        y0_filt = filter(bpf_0,y((i-sLen+1):i));
+        y0=sum(y0_filt.^2);
+        y1_filt = filter(bpf_1,y((i-sLen+1):i));
+        y1=sum(y1_filt.^2);
+%         figure;
+%         plot(linspace(0,fs,10^6),10*log10(abs(fft(y0_filt,10^6))));
+%         hold on
+%         plot(linspace(0,fs,10^6),10*log10(abs(fft(y1_filt,10^6))));
+        if(y1<y0)
+            a=0;
+        elseif(y1>y0)
+            a=1;
         end
+        y_dem=[y_dem a];
+     end
+    
+elseif(modo == 3)   
+
+        
+elseif(modo == 4)
+    Bn = 25;
+    y_dem = [];
+    sLen = length(t);    
+    [theta] = pll2(y, f0, fs, Bn);
+%     for i=sLen:sLen:length(y)
+%         [theta] = pll2(y((i-sLen+1):i), f0, fs, Bn);
+%         [theta] = pll2(y((i-sLen+1):i), f1, fs, Bn);
+%         if(sum(error0.^2)<sum(error1.^2))
+%             y_dem = [y_dem 0];
+%         else
+%             y_dem = [y_dem 1];
+%         end
     end
 end
 
-if(modo == 2)
-%     F0_l=fc-175-((564.48/2)*(1+0.7));
-%     F0_h=fc+175-((564.48/2)*(1-0.7));
-%     F1_l=fc-175+((564.48/2)*(1-0.7));
-%     F1_h=fc+175+((564.48/2)*(1+0.7));
-
-    F1_l=fc-Rb/2-Rb/2;
-    F1_h=fc-Rb/2+Rb/2;
-    F0_l=fc+Rb/2-Rb/2;
-    F0_h=fc+Rb/2+Rb/2;
-    
-N      = 4;         % Order
-Apass  = 1;         % Passband Ripple (dB)
-Astop  = 10;        % Stopband Attenuation (dB)
-
-% Construct an FDESIGN object and call its ELLIP method.
-h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F0_l, F0_h, ...
-                      Astop, Apass, Astop, fs);
-bpf_0 = design(h, 'ellip');
-h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F1_l, F1_h, ...
-                      Astop, Apass, Astop, fs);
-bpf_1 = design(h, 'ellip');
-
-    sLen = length(t);
-     y_dem =[];
-    
- for i=sLen:sLen:length(y)
-        y0_filt = filter(bpf_0,y((i-sLen+1):i));
-       y0=sum(y0_filt.^2);
-         y1_filt = filter(bpf_1,y((i-sLen+1):i));
-       y1=sum(y1_filt.^2);
-%        figure;
-%        plot(linspace(0,fs,10^6),10*log10(abs(fft(y0_filt,10^6))));
-%        hold on
-%        plot(linspace(0,fs,10^6),10*log10(abs(fft(y1_filt,10^6))));
-
-        if(y1<y0)
-            a=0
-        elseif(y1>y0)
-            a=1
-        end
-        y_dem=[y_dem a];
- end
-
-    
-end
-
-
-end
 
