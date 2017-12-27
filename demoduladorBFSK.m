3df6bf7dc9c4b1bbd6f452bebfa4f6ed60aaa525
doncel9 <33489452+doncel9@users.noreply.github.com>
2017-12-27 16:09:52 +0100

    Cambios Filtros BPF y LPF

--- demoduladorBFSK.m
+++ demoduladorBFSK.m
@@ -1,140 +1,13 @@
-<<<<<<< HEAD
 function [ y_dem ] = demoduladorBFSK(y, Rb,f0,f1,fc,modo)
 
 % Frecuencia de Muestreo
 fs = 56*Rb;
 Ts = 1/fs;
-
 Tb = 1/Rb;
 t = 0:(Ts):(Tb-Ts);
-
-if(modo == 1)
-    y_dem =[];
-    sLen = length(t);
-    for i=sLen:sLen:length(y)
-        s_dem1=cos(2*pi*f1*t);
-        s_dem0=cos(2*pi*f0*t);
-
-        y_dem1=s_dem1.*y((i-sLen+1):i);
-        y_dem0=s_dem0.*y((i-sLen+1):i);
-
-        z1=sum(y_dem1)/length(y_dem1);
-        z0=sum(y_dem0)/length(y_dem0);
-
-        if(z1>z0)
-            a=1;
-        elseif(z1<z0)
-            a=0;
-        end
-        y_dem=[y_dem a];
-    end
-elseif(modo == 2)
-    %     F0_l=fc-175-((564.48/2)*(1+0.7));
-    %     F0_h=fc+175-((564.48/2)*(1-0.7));
-    %     F1_l=fc-175+((564.48/2)*(1-0.7));
-    %     F1_h=fc+175+((564.48/2)*(1+0.7));
-
-    F1_l=fc-Rb/2-Rb/2;
-    F1_h=fc-Rb/2+Rb/2;
-    F0_l=fc+Rb/2-Rb/2;
-    F0_h=fc+Rb/2+Rb/2;
-
-    N      = 4;         % Order
-    Apass  = 1;         % Passband Ripple (dB)
-    Astop  = 10;        % Stopband Attenuation (dB)
-
-    h1  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F0_l, F0_h, ...
-                          Astop, Apass, Astop, fs);
-    bpf_0 = design(h1, 'ellip');
-    
-    
-    h2  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F1_l, F1_h, ...
-                          Astop, Apass, Astop, fs);
-    bpf_1 = design(h2, 'ellip');
-    
-    sLen = length(t);
-    y_dem =[];
-    
-    for i=sLen:sLen:length(y)
-        y0_filt = filter(bpf_0,y((i-sLen+1):i));
-        y0=sum(y0_filt.^2);
-        y1_filt = filter(bpf_1,y((i-sLen+1):i));
-        y1=sum(y1_filt.^2);
-%         figure;
-%         plot(linspace(0,fs,10^6),10*log10(abs(fft(y0_filt,10^6))));
-%         hold on
-%         plot(linspace(0,fs,10^6),10*log10(abs(fft(y1_filt,10^6))));
-        if(y1<y0)
-            a=0;
-        elseif(y1>y0)
-            a=1;
-        end
-        y_dem=[y_dem a];
-    end
-
-elseif(modo == 3)
-    y_dem =[];
-    x = Ts:Ts:length(y)/fs;
-    LO_1 = cos(2*pi*f1*x);
-    LO_0 = cos(2*pi*f0*x);
-
-    dem1 = y.*LO_1;
-    dem0 = y.*LO_0;
-
-    N     = 4;       % Order
-    Fpass = Rb;      % Passband Frequency
-    Apass = 1;       % Passband Ripple (dB)
-    Astop = 80;      % Stopband Attenuation (dB)
-
-    h = fdesign.lowpass('n,fp,ap,ast', N, Fpass, Apass, Astop, fs);
-    bpf = design(h, 'ellip');
-    dem0_filt = filter(bpf,dem0);  
-    dem1_filt = filter(bpf,dem1); 
-    
-    for k=Tb*fs:Tb*fs:length(y)
-        if (dem1_filt(k)>dem0_filt(k))
-            a=0;
-        elseif(dem1_filt(k)<dem0_filt(k))
-            a=1;
-        end
-        y_dem=[y_dem a];       
-    end
-    
-elseif(modo == 4)
-    Bn = 10;
-    y_dem = [];
-    sLen = length(t);
-    
-    [theta] = pll2(y, f1, fs, Bn);
-    [theta] = pll2(y, f0, fs, Bn);
-%     y_dem = y;
-%     for i=sLen:sLen:length(y)
-%         [theta,error0] = pll3(y((i-sLen+1):i), f0, fs, Bn);
-%         [theta,error1] = pll3(y((i-sLen+1):i), f1, fs, Bn);
-%         if(sum(error0.^2)<sum(error1.^2))
-%             y_dem = [y_dem 0];
-%         else
-%             y_dem = [y_dem 1];
-%         end
-%     end
- end
-
-end
-
-
-=======
-function [ y_dem ] = demoduladorBFSK(y, Rb,f0,f1,fc,modo)
-
-% Frecuencia de Muestreo
-fs = 56*Rb;
-
-Ts = 1/fs;
-
-Tb = 1/Rb;
-t = 0:(Ts):(Tb-Ts);
-
 sLen = length(t);
 
+%% MODO 1: DEMODULACION SIMPLE
 if(modo == 1)
     y_dem =[];
     sLen = length(t);
@@ -155,6 +28,8 @@ if(modo == 1)
         end
         y_dem=[y_dem a];
     end
+    
+%% MODO 2: FILTROS PASO BANDA SINTONIZADOS  
 elseif(modo == 2)
     %     F0_l=fc-175-((564.48/2)*(1+0.7));
     %     F0_h=fc+175-((564.48/2)*(1-0.7));
@@ -167,29 +42,31 @@ elseif(modo == 2)
     F0_h=fc+Rb/2+Rb/2;
 
 
-N      = 20;      % Order
-Apass  = 0.1;      % Passband Ripple (dB)
-Astop  = 80;     % Stopband Attenuation (dB)
+    N      = 20;      % Order
+    Apass  = 0.1;      % Passband Ripple (dB)
+    Astop  = 80;     % Stopband Attenuation (dB)
 
-  % Construct an FDESIGN object and call its ELLIP method.
- h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F0_l, F0_h, Astop, Apass, Astop, fs);
-    bpf_0 = design(h, 'ellip');
-    
-    h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F1_l, F1_h, ...
-                          Astop, Apass, Astop, fs);
+    % Filtros paso banda en ambas frecuencias
+    h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F0_l, F0_h, Astop, Apass, Astop, fs);
+    bpf_0 = design(h, 'ellip');    
+    h  = fdesign.bandpass('N,Fp1,Fp2,Ast1,Ap,Ast2', N, F1_l, F1_h, Astop, Apass, Astop, fs);
     bpf_1 = design(h, 'ellip');
     
+    % Filtro paso bajo
     h = fdesign.lowpass('n,fp,ap,ast', N, Fpass, Apass, Astop, fs);
-    bpf = design(h, 'ellip');
+    lpf = design(h, 'ellip');
     
-    y0 = filter(bpf,y0);
-    y1 = filter(bpf,y1);
+    y0 = filter(bpf_0,y);
+    y1 = filter(bpf_1,y);
+    
+    y0 = filter(lpf,y0);
+    y1 = filter(lpf,y1);
     
     y_dem = y1 - y0;
     
     rect = [zeros(1,sLen) ones(1,sLen) zeros(1,sLen)];
-    sync =  srrc(sLen*2, 0.3, 1, 0);
-    y_conv = conv(y_dem,sync); %NO MEJORA
+%     sync =  srrc(sLen*2, 0.3, 1, 0);
+    y_conv = conv(y_dem,rect); %NO MEJORA
     
     
 %    y_dem =[];
@@ -212,6 +89,7 @@ Astop  = 80;     % Stopband Attenuation (dB)
 %         y_dem=[y_dem a];
 %     end
 
+%% MODO 3: DEMODULACIÓN Y FILTROS PASO BANDA SINTONIZADOS
 elseif(modo == 3)
     y_dem =[];
     x = Ts:Ts:length(y)/fs;
@@ -239,7 +117,8 @@ elseif(modo == 3)
         end
         y_dem=[y_dem a];       
     end
-    
+
+%% MODO 4: UTILIZACION DEL PLL
 elseif(modo == 4)
     Bn = 10;
     y_dem = [];
@@ -251,6 +130,3 @@ elseif(modo == 4)
  end
 
 end
-
-
->>>>>>> 21a2959030022cc6157500d3b7460318f76920de
