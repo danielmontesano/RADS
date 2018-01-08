09d1d950c46ff46a0a249573b0e10d2690a0bcd1
Daniel Montesano <daniel.montesano@gmail.com>
2018-01-08 17:48:16 +0100

    Clockrec nuevo

--- clockrec.m
+++ clockrec.m
@@ -4,7 +4,7 @@ Ts = 1/fs;
 Tb = 1/Rb;
 t = 0:(Ts):(Tb-Ts);
 
-l=length(t); 
+l=length(t)/2; 
 m=56;
 x=r;
 % matchfilt=srrc(l,beta,m,0);
@@ -14,27 +14,27 @@ x=r;
 % else
 %     tnow = m*m+1;
 % end
-tnow = round(m/2);
+tnow = l*2;
 tau=0; 
 tausave(1)=tau; i=0;
 mu=1.5;                            % algorithm stepsize
 delta=1;                          % time for derivative
-while tnow<length(x)              % run iteration
+while tnow<length(x)-56              % run iteration
   i=i+1;
   
-  xs(i) = interp1([floor(tnow+tau) (floor(tnow+tau)+1)],...
-                  [x(floor(tnow+tau)) x(floor(tnow+tau)+1)],...
-                  [tnow+tau]);
-  x_deltap = interp1([floor(tnow+tau+delta) (floor(tnow+tau+delta)+1)],...
-                  [x(floor(tnow+tau+delta)) x(floor(tnow+tau+delta)+1)],...
-                  [tnow+tau+delta]);
-  x_deltam = interp1([floor(tnow+tau-delta) (floor(tnow+tau-delta)+1)],...
-                  [x(floor(tnow+tau-delta)) x(floor(tnow+tau-delta)+1)],...
-                  [tnow+tau-delta]);
-              
-%   xs(i)=interpsinc(x,tnow+tau,l);   % interp value at tnow+tau
-%   x_deltap=interpsinc(x,tnow+tau+delta,l); % value to right
-%   x_deltam=interpsinc(x,tnow+tau-delta,l); % value to left
+%   xs(i) = interp1([floor(tnow+tau) (floor(tnow+tau)+1)],...
+%                   [x(floor(tnow+tau)) x(floor(tnow+tau)+1)],...
+%                   [tnow+tau]);
+%   x_deltap = interp1([floor(tnow+tau+delta) (floor(tnow+tau+delta)+1)],...
+%                   [x(floor(tnow+tau+delta)) x(floor(tnow+tau+delta)+1)],...
+%                   [tnow+tau+delta]);
+%   x_deltam = interp1([floor(tnow+tau-delta) (floor(tnow+tau-delta)+1)],...
+%                   [x(floor(tnow+tau-delta)) x(floor(tnow+tau-delta)+1)],...
+%                   [tnow+tau-delta]);
+%               
+  xs(i)=interpsinc(x,tnow+tau,l);   % interp value at tnow+tau
+  x_deltap=interpsinc(x,tnow+tau+delta,l); % value to right
+  x_deltam=interpsinc(x,tnow+tau-delta,l); % value to left
 
   dx=x_deltap-x_deltam;             % numerical derivative
   qx=quantalph(xs(i),[-1,1]);  % quantize to alphabet
